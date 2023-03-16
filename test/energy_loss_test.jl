0d151882c8fbcd263c29ec7fa4367b81ed5344a6
Joe Fowler <drjoefowler@gmail.com>
2023-03-16 08:57:28 -0600

    Fix energy loss tests

--- test/energy_loss_test.jl
+++ test/energy_loss_test.jl
@@ -1,20 +1,24 @@
 using MuscRat
 
+import Unitful.MeV
+MeV_over_c = 1u"MeV/c"
+MeV_per_cm = 1u"MeV/cm"
+
 @testset "energy loss" begin
     tests = (
-        (:Silicon, :µ, 300, 3.9494),
-        (:Copper, :µ, 300, 13.06),
-        (:NaI, :µ, 300, 5.0),
-        (:Silicon, :π, 300, 4.06446),
-        (:Silicon, :p, 3000, 3.94683),
-        (:Silicon, :e, 1, 3.7292),
+        (:Silicon, :µ,  300MeV_over_c, 3.9494MeV_per_cm),
+        (:Copper,  :µ,  300MeV_over_c, 13.06MeV_per_cm),
+        (:NaI,     :µ,  300MeV_over_c, 5.0MeV_per_cm),
+        (:Silicon, :π,  300MeV_over_c, 4.06446MeV_per_cm),
+        (:Silicon, :p, 3000MeV_over_c, 3.94683MeV_per_cm),
+        (:Silicon, :e,    1MeV_over_c, 3.7292MeV_per_cm),
     )
     for t in tests
-        material, particle, p_MeV, loss = t
+        material, particle, momentum, loss = t
         f = Eloss_function(material, particle)
-        @test isapprox(f(p_MeV), loss; rtol=.001)
+        @test isapprox(f(momentum), loss; rtol=.001)
     end
     f = Eloss_function()
-    @test f(300) < f(1000)
-    @test f(300) < f(100)
+    @test f(300MeV_over_c) < f(1000MeV_over_c)
+    @test f(300MeV_over_c) < f(100MeV_over_c)
 end
