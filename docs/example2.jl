bc70ab9252d16b029c258f2902360c62f34326f9
Joe Fowler <drjoefowler@gmail.com>
2023-04-12 16:00:50 -0600

    Update example2.jl

--- docs/example2.jl
+++ docs/example2.jl
@@ -1,15 +1,16 @@
-using PyPlot, MuscRat, Statistics
+using PyPlot, Unitful, MuscRat, Statistics
 
 particles = (
-    MuscRat.Gamma,
     MuscRat.µplus,
     MuscRat.µminus,
     MuscRat.Electron,
     MuscRat.Positron,
+    MuscRat.Gamma,
 )
+masses =
 
-TKID = Box([.5, .5, .05])
-area = MuscRat.tube_area(TKID)*1u"cm^2"
+TKID = Box([5, 5, 1.5]*u"mm")
+area = MuscRat.tube_area(TKID)
 Ttotal = 1e8u"s"
 Pmin = 10.0u"MeV/c"
 
@@ -21,4 +22,15 @@ for particle in particles
     N = Int(round(Ttotal*rate))
     println("Generating $N $particle cosmic rays...")
     p[particle], cosθ[particle] = generate(g, N)
+end
+clf(); loglog()
+xlabel("Energy (GeV)")
+for particle in particles
+    mass = MuscRat.masses[particle]
+    EGeV = p[particle]/1u"GeV/c"
+    if mass > 0u"MeV/c^2"
+        EGeV = mass/1u"GeV/c^2"*(sqrt.(1 .+ (p[particle]/mass/Unitful.c) .^ 2) .- 1)
+    end
+    EGeV = uconvert.(NoUnits, EGeV)
+    hist(EGeV, 1000, [0, 100], histtype="step")
 end
