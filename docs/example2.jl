54ec01d84822a7147bb2fd3ac9534c450dba43e0
Joe Fowler <drjoefowler@gmail.com>
2023-04-13 10:20:16 -0600

    Use weighted sampling from different particles

--- docs/example2.jl
+++ docs/example2.jl
@@ -7,23 +7,39 @@ particles = (
     MuscRat.Positron,
     MuscRat.Gamma,
 )
+weights = Dict(
+    MuscRat.µplus => 1,
+    MuscRat.µminus => 1,
+    MuscRat.Electron => 30,
+    MuscRat.Positron => 10,
+    MuscRat.Gamma => 3000,
+)
+Pmin = Dict(
+    MuscRat.µplus => 10.0u"MeV/c",
+    MuscRat.µminus => 10.0u"MeV/c",
+    MuscRat.Electron => 100.0u"MeV/c",
+    MuscRat.Positron => 100.0u"MeV/c",
+    MuscRat.Gamma => 300.0u"MeV/c",
+)
 
 TKID = Box([5, 5, 1.5]*u"mm")
 area = MuscRat.tube_area(TKID)
-Ttotal = 1e8u"s"
-Pmin = 10.0u"MeV/c"
+Ttotal = 1e7u"s"
 
 p = Dict{MuscRat.Particle,Vector}()
 cosθ = Dict{MuscRat.Particle,Vector}()
 for particle in particles
-    g = ParmaGenerator(particle; Pmin=Pmin)
+    g = ParmaGenerator(particle; Pmin=Pmin[particle])
     rate = g.flux*area
-    N = Int(round(Ttotal*rate))
-    println("Generating $N $particle cosmic rays...")
+    weight = weights[particle]
+    N = Int(round(Ttotal*rate/weight))
+    println("Generating $N $particle cosmic rays with weight $weight...")
     p[particle], cosθ[particle] = generate(g, N)
 end
+
 clf(); loglog()
 xlabel("Energy (GeV)")
+ylabel("CR flux (counts per cm^2 per second per MeV)")
 for particle in particles
     mass = MuscRat.masses[particle]
     EGeV = p[particle]/1u"GeV/c"
@@ -31,5 +47,8 @@ for particle in particles
         EGeV = mass/1u"GeV/c^2"*(sqrt.(1 .+ (p[particle]/mass/Unitful.c) .^ 2) .- 1)
     end
     EGeV = uconvert.(NoUnits, EGeV)
-    hist(EGeV, 1000, [0, 100], histtype="step")
+    binwidth = 100 # MeV
+    w = ones(length(EGeV))*(1u"s"/Ttotal)*weights[particle]/binwidth/0.25
+    hist(EGeV, 1000, [0, 100], histtype="step", weights=w, label=string(particle))
 end
+legend()
