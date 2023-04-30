177ddbe7582718bdab950fa10cd6af7dce0c653a
Joe Fowler <drjoefowler@gmail.com>
2023-04-30 13:22:32 -0600

    Update example_compare_muons.jl

--- docs/example_compare_muons.jl
+++ docs/example_compare_muons.jl
@@ -1,67 +1,73 @@
 using PyPlot, Unitful, MuscRat, Statistics
 
-particles = (
-    MuscRat.µplus,
-    MuscRat.µminus,
-)
+Ttotal = 1e8u"s"
 
-TKID = Box([5, 5, 1.5]*u"mm")
-area = MuscRat.tube_area(TKID)
-Ttotal = 5e7u"s"
-Pmin = 10.0u"MeV/c"
-loss_in_concrete = 0.0u"MeV"
+function run_simulation()
+    TKID = Box([5, 5, 1.5]*u"mm")
+    area = MuscRat.tube_area(TKID)
+    Pmin = 10.0u"MeV/c"
+    loss_in_concrete = 0.0u"MeV"
 
-pparma = Dict{MuscRat.Particle,Vector}()
-cosθparma = Dict{MuscRat.Particle,Vector}()
-for particle in particles
-    g = ParmaGenerator(particle; Pmin=Pmin)
-    rate = g.flux*area
+    particles = (
+        MuscRat.µplus,
+        MuscRat.µminus,
+    )
+    pparma = Dict{MuscRat.Particle,Vector}()
+    cosθparma = Dict{MuscRat.Particle,Vector}()
+    for particle in particles
+        g = ParmaGenerator(particle; Pmin=Pmin)
+        rate = g.flux*area
+        N = Int(round(Ttotal*rate))
+        println("Generating $N $particle cosmic rays...")
+        @time pparma[particle], cosθparma[particle] = generate(g, N)
+    end
+
+    p = vcat(pparma[MuscRat.µplus], pparma[MuscRat.µminus])
+    e = MuscRat.muon_E.(p) .- loss_in_concrete
+    e[e .< 0u"GeV"] .= 0u"GeV"
+    p = MuscRat.muon_p.(e)
+    allp = [p]
+    allcosθ = [vcat(cosθparma[MuscRat.µplus], cosθparma[MuscRat.µminus])]
+    algorithms = ["PARMA"]
+
+    Pmin = uconvert(u"GeV/c", Pmin)
+    generator = CRMuonGenerator(100, 100; Pmin=Pmin, useReyna=true);
+    rate = generator.flux*area
     N = Int(round(Ttotal*rate))
-    println("Generating $N $particle cosmic rays...")
-    @time pparma[particle], cosθparma[particle] = generate(g, N)
+    println("Generating $N cosmic rays from Reyna...")
+    @time p,cosθ = generate(generator, N);
+    e = MuscRat.muon_E.(p) .- loss_in_concrete
+    e[e .< 0u"GeV"] .= 0u"GeV"
+    p = MuscRat.muon_p.(e)
+    push!(allp, p)
+    push!(allcosθ, cosθ)
+    push!(algorithms, "Reyna")
+
+    for depth in (1000u"g/cm^2", 850u"g/cm^2")
+        generator = CRMuonGenerator(100, 100; Pmin=Pmin, useReyna=false, y=depth);
+        rate = generator.flux*area
+        N = Int(round(Ttotal*rate))
+        println("Generating $N cosmic rays from Chatzidakis at depth $depth...")
+        @time p,cosθ = generate(generator, N);
+        e = MuscRat.muon_E.(p) .- loss_in_concrete
+        e[e .< 0u"GeV"] .= 0u"GeV"
+        p = MuscRat.muon_p.(e)
+        push!(allp, p)
+        push!(allcosθ, cosθ)
+        push!(algorithms, "Chatzidakis $depth")
+    end
+
+    println("Computing path lengths")
+    @time total_paths = [MuscRat.path_values(TKID, cosθ) for cosθ in allcosθ]
+    println("Computing energy loss given paths")
+
+    loss_distribution = MuscRat.Eloss_µSi()
+    @time Loss_tkid = [loss_distribution.(allp[i], total_paths[i]) for i=1:4]
+    allp, allcosθ, algorithms, total_paths, Loss_tkid
 end
+allp, allcosθ, algorithms, total_paths, Loss_tkid = run_simulation()
 
-p = vcat(pparma[MuscRat.µplus], pparma[MuscRat.µminus])
-e = MuscRat.muon_E.(p) .- loss_in_concrete
-e[e .< 0u"GeV"] .= 0u"GeV"
-p = MuscRat.muon_p.(e)
-allp = [p]
-allcosθ = [vcat(cosθparma[MuscRat.µplus], cosθparma[MuscRat.µminus])]
-algorithms = ["PARMA"]
-
-Pmin = uconvert(u"GeV/c", Pmin)
-generator = CRMuonGenerator(100, 100; Pmin=Pmin, useReyna=true);
-rate = generator.flux*area
-N = Int(round(Ttotal*rate))
-println("Generating $N cosmic rays from Reyna...")
-@time p,cosθ = generate(generator, N);
-e = MuscRat.muon_E.(p) .- loss_in_concrete
-e[e .< 0u"GeV"] .= 0u"GeV"
-p = MuscRat.muon_p.(e)
-push!(allp, p)
-push!(allcosθ, cosθ)
-push!(algorithms, "Reyna")
-
-generator = CRMuonGenerator(100, 100; Pmin=Pmin, useReyna=false, y=1000.0u"g/cm^2");
-rate = generator.flux*area
-N = Int(round(Ttotal*rate))
-println("Generating $N cosmic rays from Chatzidakis...")
-@time p,cosθ = generate(generator, N);
-e = MuscRat.muon_E.(p) .- loss_in_concrete
-e[e .< 0u"GeV"] .= 0u"GeV"
-p = MuscRat.muon_p.(e)
-push!(allp, p)
-push!(allcosθ, cosθ)
-push!(algorithms, "Chatzidakis")
-
-println("Computing path lengths")
-@time total_paths = [MuscRat.path_values(TKID, cosθ) for cosθ in allcosθ]
-println("Computing energy loss given paths")
-
-eloss_tkid = Δprob(:Silicon, :µ)
-@time Loss_tkid = [eloss_tkid.(allp[i], total_paths[i]) for i=1:3]
-
-function plot_distributions(total_paths, Loss_tkid, lossMax=5.0)
+function plot_distributions(allp, total_paths, Loss_tkid, lossMax=5.0)
     clf()
     ax1 = subplot(311); loglog(); xlabel("µ± momentum (GeV/c)")
     ylabel("Counts / GeV/c / cm\$^2\$ / second")
@@ -74,12 +80,12 @@ function plot_distributions(total_paths, Loss_tkid, lossMax=5.0)
     Emax = 200.0
     lossbins = 500
     area = 0.5^2
-    for i=1:3
+    for i=1:4
         plen = total_paths[i]
         pGeV = allp[i]/1u"GeV/c"
         EGeV = MuscRat.muon_E.(allp[i])/1u"GeV"
         lossMeV = Loss_tkid[i][plen.>0u"cm"]/Unitful.MeV .|> NoUnits
-        lossMeV .*= exp.(randn(length(lossMeV))*0.15)
+        # lossMeV .*= exp.(randn(length(lossMeV))*0.15)
 
         sca(ax1)
         binwidth = Pmax/Nbins
@@ -98,4 +104,23 @@ function plot_distributions(total_paths, Loss_tkid, lossMax=5.0)
     end
     legend()
 end
-plot_distributions(total_paths, Loss_tkid)
+
+plot_distributions(allp, total_paths, Loss_tkid)
+
+# function save_distributions(allp, allcosθ, total_paths, Loss_tkid, algorithms)
+#     h5open("muon_CR_loss_4models.hdf5", "w") do h
+#         for (i, k) in enumerate(algorithms)
+#             name = split(k, " g ")[1]
+#             name = replace(name, " "=>"_")
+#             g = create_group(h, name)
+
+#             use = total_paths[i] .> 0u"mm"
+#             write(g, "p_GeV", allp[i][use]/1u"GeV/c")
+#             write(g, "costheta", allcosθ[i][use])
+#             write(g, "path_mm", total_paths[i][use]/1u"mm")
+#             write(g, "loss_keV", Loss_tkid[i][use]/1u"keV")
+#         end
+#     end
+# end
+
+# Ttotal <= 1.1e8u"s" && save_distributions(allp, allcosθ, total_paths, Loss_tkid, algorithms)
