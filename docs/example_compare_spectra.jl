e869cfee6524aedfce5141ffb4f9418e58c1fc2b
Joe Fowler <drjoefowler@gmail.com>
2024-08-12 09:52:52 -0600

    Add an example plot (muon flux at 5 elevations)

--- docs/example_compare_spectra.jl
+++ docs/example_compare_spectra.jl
@@ -41,3 +41,19 @@ function plot_all_spectra()
     xlabel("Energy (GeV)")
     ylabel("Spectrum (counts per cm\$^2\$ per s per LOGARITHMIC E BIN)")
 end
+
+
+function plot_muons_elevation()
+    clf()
+
+    for y0 in LinRange(850, 1030, 5)
+        gc = CRMuonGenerator(100, 100; y=y0*u"g/cm^2", Pmin=0.045u"GeV/c", useReyna=false)
+        P_GeV = exp.(gc.logPGeVlim[2:end])
+        E_GeV = MuscRat.muon_E.(P_GeV*1u"GeV/c")/1u"GeV" .|> NoUnits
+        chatz = sum(gc.relativeprob, dims=2)*(gc.flux*1u"s*cm^2" |> NoUnits)
+        loglog(E_GeV, chatz, "-", label="Chatzidakis")
+        @show sum(chatz)
+    end
+    xlabel("Energy (GeV)")
+    ylabel("Flux (counts per s per cm^2)")
+end
